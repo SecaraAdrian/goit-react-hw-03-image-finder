57a23f99f8cb6578b8574f3914f7a64103e02070
SecaraAdrian <secaforce@gmail.com>
2024-07-15 17:02:57 +0300

    Commit local changes before merging gh-pages into main

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import Loader from 'react-loader-spinner'; 
+import Loader from 'react-loader-spinner';
+import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
 import '../index.css';
 
 const Spinner = () => (
   <div className="LoaderOverlay">
     <Loader
-      type="Circles"
+      type="Rings"  
       color="#3f51b5"
       height={100}
       width={100}
